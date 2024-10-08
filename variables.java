4021eaba7de35a731c027062826f0a03e1f809b0
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-08 14:44:56 +0530

    variables has been added

---
+++ variables.java
@@ -0,0 +1,19 @@
+public class variables {
+    public static void main(String[] args){
+        int x = 123; //initialization
+        System.out.println("My number is: "+ x);
+
+        long y = 12387448589L;
+        System.out.println("My long number is: "+ y);
+
+        double z= 7.234;
+        System.out.println("My sum number is: "+ y+z); 
+        // doesnt add instead concatenates 
+
+        char symbol = '@';
+
+        String name = "Diya";
+        System.out.println("My name is: "+ name);
+    
+    }
+}
