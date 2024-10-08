61b49c464224bd45bf5fa4b5f349f251c42d1b20
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-09 00:02:45 +0530

    Adeed user inputs

--- user_input.java
+++ user_input.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class user_input {
     public static void main(String[] args){
 
-        Scanner inpput = new Scanner(System.in);
+        Scanner enter = new Scanner(System.in);
         System.out.println("What is your name?  ");
-        String name = inpput.nextLine();
+        String name = enter.nextLine();
         System.out.println("Hello, " + name + "!");
 
         System.out.println("How old are you?  ");
-        int age = inpput.nextInt();
+        int age = enter.nextInt();
         System.out.println("You are "+ age +"years old.");
 
-        inpput.nextLine(); //to remove extra \n left ater nextInt()
+        enter.nextLine(); //to remove extra \n left ater nextInt()
 
         System.out.println("What is your hobby?  ");
-        String hobby = inpput.nextLine();
+        String hobby = enter.nextLine();
         System.out.println("I love " + hobby + "!:)");
     }
 }
