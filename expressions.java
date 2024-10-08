2a583160db1029b4b8b70f22f33be8665346ab28
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-08 23:31:11 +0530

    added expressions

---
+++ expressions.java
@@ -0,0 +1,22 @@
+public class expressions {
+    public static void main(String[] args){
+        
+
+        int friends = 10;
+
+        friends++;
+        friends--;
+        friends += 2;
+        friends -= 2;
+        friends *= 2;
+        friends /= 3;
+        friends %= 3;
+
+        double new = 10;
+        new /= 3;
+
+        System.out.println("Friends after expressions: " + friends);
+        System.out.println("new after expressions: " + new);
+
+    }
+}
