4387cdee83f30edd7fffdf2e0b51a8e30dc42035
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-09 02:42:35 +0530

    added program to demonstrate arrays

---
+++ arrays.java
@@ -0,0 +1,17 @@
+public class arrays {
+    public static void main(String[] args){
+        String[] cars = {"CAMARO", "CORVETTE", "TESLA"};
+
+        cars[0] = "Mustang";
+        System.out.println(cars[1]);
+
+        String[] flowers = new String[3];
+        flowers[0] = "ROSE";
+        flowers[1] = "LILY";
+        flowers[2] = "DAISY";
+
+        for(int i =0,i<3){
+        System.out.println(flowers[i]);
+        }
+    }
+}
