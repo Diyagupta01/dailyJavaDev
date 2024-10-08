2012cc8df831caa65b6e1886272ac1d68c699022
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-08 23:30:52 +0530

    added expressions

--- user_input.java
+++ user_input.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class user_input {
     public static void main(String[] args){
 
-        Scanner input = new Scanner(System.in);
+        Scanner inpput = new Scanner(System.in);
         System.out.println("What is your name?  ");
-        String name = input.nextLine();
+        String name = inpput.nextLine();
         System.out.println("Hello, " + name + "!");
 
         System.out.println("How old are you?  ");
-        int age = input.nextInt();
+        int age = inpput.nextInt();
         System.out.println("You are "+ age +"years old.");
 
-        input.nextLine(); //to remove extra \n left ater nextInt()
+        inpput.nextLine(); //to remove extra \n left ater nextInt()
 
         System.out.println("What is your hobby?  ");
-        String hobby = input.nextLine();
+        String hobby = inpput.nextLine();
         System.out.println("I love " + hobby + "!:)");
     }
 }
