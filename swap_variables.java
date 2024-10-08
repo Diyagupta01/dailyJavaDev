eec9de57483c4ffa4c4d51bf3f5dee06c2dd0db5
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-09 00:06:09 +0530

    added program to swap variables

--- swap_variables.java
+++ swap_variables.java
@@ -1,18 +1,18 @@
 public class swap_variables {
     public static void main(String[] args){
-        String x = "Water";
+        String x1 = "Water";
         String y = "Juice";
         String temp;
 
         System.out.println("Before Swapping:- ");
-        System.out.println("x: "+x);
+        System.out.println("x: "+x1);
         System.out.println("y: "+y);
 
         System.out.println("After Swapping:- ");
-        temp = x;
-        x=y;
+        temp = x1;
+        x1=y;
         y=temp;
-        System.out.println("x: "+x);
+        System.out.println("x: "+x1);
         System.out.println("y: "+y);
     }
 }
