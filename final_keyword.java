bd8b90c97dd13c985c48e5a4871d1deeee9c0932
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-09 18:54:54 +0530

    added program using final keyword

---
+++ final_keyword.java
@@ -0,0 +1,10 @@
+public class final_keyword {
+   public static void main(String[] args) {
+    //cant be changed or updates
+
+    final double PI = 3.14159;
+//PI = 4;  //not allowed
+System.out.println(PI);
+
+   } 
+}
