a44c1308f64830ddf489f28a0346fadc5e2de1cc
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-09 22:21:52 +0530

    added program demostrating oops

---
+++ oops/ooops.java
@@ -0,0 +1,13 @@
+
+package oops;
+public class ooops {
+   public static void main(String[] args) {
+
+    car mycar = new car();
+
+   System.out.println(mycar.make);
+
+   mycar.drive();
+
+   }
+}
