f0345e869f19a331d58a6e97f688d9c786024449
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-09 11:43:41 +0530

    added wrapper class

---
+++ wrapper_class.java
@@ -0,0 +1,22 @@
+public class wrapper_class {
+   public static void main(String[] args) {
+ 
+    //wrapper class : provides a way to use primitive data types as refernence data types as they contains useful methods that can be used with collections (ex: ArrayList)
+    //primitive   //wrapper
+    //----------------------
+    //int          //Integer
+    //double       //Double
+    //boolean      //Boolean
+    //char         //Character
+
+// autoboxing = the automatic conversion that the Java compiler makes between the primitive type and reference type
+//unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+
+Boolean a = true;
+Character b = '@';
+Integer C = 123;
+Double d = 3.14;
+String e = "Diyaaaa";
+    
+   }
+}
