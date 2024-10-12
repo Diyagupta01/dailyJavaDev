a93f3e1f3cceca38ac921d1aaf557f83b5144e82
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-12 09:59:57 +0530

    Added a diceeroller program to semonstrate variable scopes.

---
+++ oops/diceRoller.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class diceRoller {
+
+    Random random;
+    int number = 0;
+     
+    diceRoller(){
+        random = new Random();
+        roll();
+    }
+
+    void roll(){
+        number = random.nextInt(6)+1;
+        System.out.println("You rolled a " + number);
+    }
+}
---
+++ oops/var_scopes.java
@@ -0,0 +1,5 @@
+public class var_scopes {
+    public static void main(String[] args) {
+        diceRoller diceroller = new diceRoller();
+    }
+}
