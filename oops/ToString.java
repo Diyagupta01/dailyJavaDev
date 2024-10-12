c866a17d0d37f1ce35e7b21c1e0faa2c1946c98c
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-12 20:53:01 +0530

    Added a program to demonstrate using toString method

---
+++ oops/ToString.java
@@ -0,0 +1,17 @@
+public class ToString {
+
+    public static void main(String[] args) {
+        car_tostring car =  new car_tostring();
+
+//    System.out.println(car.make);
+//    System.out.println(car.model);
+//    System.out.println(car.colour);
+//    System.out.println(car.year);
+
+   //System.out.println(car.toString());
+   System.out.println(car);
+
+    }
+
+   
+}
---
+++ oops/car_tostring.java
@@ -0,0 +1,14 @@
+public class car_tostring {
+
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2021;
+    String colour = "red";
+
+    public String toString(){
+
+        String mystring =  "Make: " + make + "\nModel: " + model + "\nYear: "
+        + year + "\nColour: " + colour;
+        return mystring;
+    }
+}
