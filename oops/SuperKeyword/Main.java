66eca898f8cc92c699f0dfcb8e0b0ecc99fecff5
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-14 16:30:32 +0530

    Added a program to demonstrate using Super Keyword

---
+++ oops/SuperKeyword/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("Btaman",42,"$$$");
+        Hero hero2 = new Hero("Superman",38,"Everything");
+        System.out.println("Hero1 details:");
+        System.out.println(hero1.toString());
+        System.out.println("\nHero2 details:"); 
+        System.out.println(hero2.toString());
+        
+    }
+}
