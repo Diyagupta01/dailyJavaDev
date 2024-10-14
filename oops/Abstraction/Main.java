56a0fc94aef84cf9389092c7e79551fdae2cd60d
Diyagupta01 <dgupta60_be22@thapar.edu>
2024-10-14 16:42:52 +0530

    Added a program to demonstrate concept of abstract class

---
+++ oops/Abstraction/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Car car = new Car();
+        //Vehicle veh = new Vehicle(); //Cannot instantiate the type Vehicle (ABSTRACT)
+
+        car.go();
+    }
+}
